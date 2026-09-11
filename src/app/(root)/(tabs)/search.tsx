2af822228b94104e526ede469345bc7bb14a6eae
Victor Gbadebo <victorgbadebo10@gmail.com>
2026-09-11 07:11:06 +0100

    initial commits and setup

---
+++ src/app/(root)/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Search = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Search</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Search;
